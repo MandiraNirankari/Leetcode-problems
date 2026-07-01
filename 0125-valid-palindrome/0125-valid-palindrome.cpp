5602a492a78078a31066599c97620c35e4b95940
MandiraNirankari <mandiranirankari@gmail.com>
2026-07-01 16:05:39 +0530

    Time: 4 ms (14.66%), Space: 10 MB (38.45%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAlphaNum(char ch){
+        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int st = 0, e = s.length() - 1;
+
+        while(st<e){
+            if(!isAlphaNum(s[st])){
+                st++; 
+                continue;
+            }
+            if(!isAlphaNum(s[e])){
+                e--;
+                continue;
+            }
+            if(tolower(s[st])!=tolower(s[e])){
+                return false;
+            }
+            st++; e--;
+        }
+    
+        return true;
+    }
+};

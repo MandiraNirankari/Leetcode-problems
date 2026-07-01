cb692996bd04531c3b00f6adf91f6ac586549cfa
MandiraNirankari <mandiranirankari@gmail.com>
2026-07-01 16:01:52 +0530

    Time: 3 ms (45.19%), Space: 8.4 MB (92.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(x>0){
+        if(rev>INT_MAX/10 || rev<INT_MIN/10){
+            return 0;
+        }
+        rev=rev*10+x%10;
+        x/=10;
+       }
+       if(temp==rev){
+            return true;
+       }else{
+        return false;
+       }
+    }
+};

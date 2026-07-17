7e40b01f011ab23f5f68a69a97deb53e2de1d48b
MandiraNirankari <mandiranirankari@gmail.com>
2026-07-17 10:20:29 +0530

    Time: 0 ms (100%), Space: 11.8 MB (5.1%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        vector<int> pop(3000);
+        for(auto i:logs){
+            pop[i[0]]++;
+            pop[i[1]]--;
+        }
+        int maxi=0, year;
+        for(int i=1950;i<2050;i++){
+            pop[i]+=pop[i-1];
+            if(pop[i]>maxi){
+                maxi=max(maxi,pop[i]);
+                year=i;
+            }
+        }
+        return year;
+    }
+};

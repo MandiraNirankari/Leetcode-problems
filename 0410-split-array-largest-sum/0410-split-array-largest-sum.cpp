db051c6298651cc7cf09123cd6011c432657b880
MandiraNirankari <mandiranirankari@gmail.com>
2026-08-08 22:45:57 +0530

    Time: 0 ms (100%), Space: 10.6 MB (10.31%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-
-    bool isPossible(vector<int>& nums, int k, int maxSum) {
-
-        int subarrays = 1;
-        int sum = 0;
-
-        for(int i = 0; i < nums.size(); i++) {
-
-            if(sum + nums[i] <= maxSum) {
-                sum += nums[i];
-            }
-            else {
-                subarrays++;
-                sum = nums[i];
+    bool isPossible(vector<int>& nums, int k, int maxsum){
+        int sum=0,subarray=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]+sum<=maxsum){
+                sum+=nums[i];
+            }else{
+                subarray++;
+                sum=nums[i];
             }
         }
-
-        return subarrays <= k;
+        return subarray<=k;
     }
-
     int splitArray(vector<int>& nums, int k) {
-
-        int low = *max_element(nums.begin(), nums.end());
-
-        int high = accumulate(nums.begin(), nums.end(), 0);
-
-        int ans = high;
-
-        while(low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if(isPossible(nums, k, mid)) {
-                ans = mid;
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(nums,k,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
         return ans;
     }
 };
